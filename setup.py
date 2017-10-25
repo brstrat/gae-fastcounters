b14d669cd506c48ad17b52017b9ae0b9cc7ba869
Troy Harvey <troyharvey@gmail.com>
2017-10-25 15:06:14 -0400

    Adding setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='fastcounter',
+      version='1.0',
+      description='Fast counter library for App Engine',
+      url='https://github.com/brstrat/gae-fastcounters',
+      author='David Underhill',
+      author_email='dgu@cs.stanford.edu',
+      license='MIT',
+      packages=['fastcounter'],
+      zip_safe=False)
